33d6d1d3a9bf3bd37bb237ea3db5c2022972e138
Mohraiel <mohraielmatta@gmail.com>
2023-10-25 14:25:12 -0700

    mui installed and visibility toggle implemented

--- src/modules/UserLogin.js
+++ src/modules/UserLogin.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 import qs from 'qs';
 import { LogInUpdate, UserInfoUpdate } from './UserInfoContext';
 import { Link, useNavigate } from 'react-router-dom';
+import { Box, Button, Input, IconButton, TextField } from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 function UserLogin(props) {
     const navigate = useNavigate();
@@ -12,55 +15,93 @@ function UserLogin(props) {
     const passwordRef = useRef();
     const updateUser = UserInfoUpdate();
     const updateLogIn = LogInUpdate();
+    const [showPassword, setShowPassword] = useState(false);
 
     useEffect(() => {
         controller = new AbortController();
-        isMounted = true
-    
+        isMounted = true;
+
         return () => {
-            isMounted = false
-            controller.abort()
-        }
-    }, [])
-    
+            isMounted = false;
+            controller.abort();
+        };
+    }, []);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword((prevShowPassword) => !prevShowPassword);
+    };
 
     const postLogin = async (e) => {
         e.preventDefault();
         let data = {
             'username': emailRef.current.value,
-            'password': passwordRef.current.value 
-        }
-        
+            'password': passwordRef.current.value
+        };
+
         let config = {
             method: 'post',
             url: 'http://localhost:4000/login',
             withCredentials: true,
-            headers: { 
+            headers: {
                 'Content-Type': 'application/json',
             },
-            data : data
+            data: data
         };
 
         axios(config)
-        .then(function (response) {
-            console.log(response.data);
-            updateUser(response.data)
-            updateLogIn(true)
-            navigate('/');
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+            .then(function (response) {
+                console.log(response.data);
+                updateUser(response.data);
+                updateLogIn(true);
+                navigate('/');
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     return (
         <form onSubmit={postLogin} id="userLoginBox" className="inputBox">
             <h2 className="formTitle">Login</h2>
             <label>Email</label>
-            <input type="email" ref={emailRef}></input>
+            <TextField
+                inputRef={emailRef}
+                variant="outlined"
+                fullWidth
+            />
+            {/* password visibility with mui's features */}
             <label>Password</label>
-            <input type="password" ref={passwordRef}></input>
-            <button className="button">Submit</button>
+            <TextField
+                type={showPassword ? 'text' : 'password'}
+                inputRef={passwordRef}
+                variant="outlined"
+                fullWidth
+                InputProps={{
+                    endAdornment: (
+                        <IconButton
+                            size="small"
+                            onClick={togglePasswordVisibility}
+                        >
+                            {showPassword ? (
+                                <VisibilityOff fontSize="small" />
+                            ) : (
+                                <Visibility fontSize="small" />
+                            )}
+                        </IconButton>
+                    ),
+                }}
+            />
+            {/* spacing it out before the submit button */}
+            <div style={{ marginTop: '10px' }}>
+                <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                >
+                    Submit
+                </Button>
+            </div>
         </form>
     )
 }
